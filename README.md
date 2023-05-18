1c582b0271c095809fd7fae28b6682c888f9a464
HakanKavci <hackankavci0@gmail.com>
2023-05-18 17:20:49 +0300

    my name added

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Hiring-Page
+# Hiring Page
+I built two responsive pages for mobile/desktop as designed in the Figma design. Codes created with mobile-first approach.
+
+## Project Skeleton
+
+|
+|----readme.md                  
+|----assets
+        |----img
+        |----Project Gif
+|----index.html  
+|----style.css   
+|----app.js  
+
+
+## Outcome
+
+## What I used
+- HTML
+- CSS
+- JavaScript
+
+
+## Notes
---
+++ mockData.js
@@ -0,0 +1,36 @@
+export const data = [
+    {
+      name1: "Lynn Tanner",
+      name2: "Sledge Hammer"
+    },
+    {
+      name1: "Roderick Flich",
+      name2: "Willie Tanner"
+    },
+    {
+      name1: "Angus MacGyver",
+      name2: "Michael Knight"
+    },
+    {
+      name1: "Hannibal Smith",
+      name2: "Fred Decker"
+    },
+    {
+      name1: "Thomas Magnum",
+      name2: "Tony Danza"
+    },
+    {
+      name1: "Roderick Decker",
+      name2: "Hakan Kavci"
+    },
+    {
+      name1: "Cristian Tanner",
+      name2: "Capt. Trunk"
+    },
+    {
+      name1: "B.A. Baracus",
+      name2: "Dori Doreau"
+    },
+    
+  ];
+  
